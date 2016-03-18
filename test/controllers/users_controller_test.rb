02ccce34ce223aa2c144295b4a0effb6294b4fa0
Jason Wu <wujiechen@gmail.com>
2016-03-18 09:34:07 +0800

    Add users list Add fake users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,11 @@ class UsersControllerTest < ActionController::TestCase
     @other_user = users(:archer)
   end
 
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+  
   test "should get new" do
     get :new
     assert_response :success
@@ -22,7 +27,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
+
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get :edit, id: @user
